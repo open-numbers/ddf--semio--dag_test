77dca8e9c62446b0e66d4b6269def5775f855abb
semio <prairy.long@gmail.com>
2018-05-17 10:58:59 +0800

    add one resource

--- etl/scripts/etl.py
+++ etl/scripts/etl.py
@@ -1,4 +1,12 @@
 # coding: utf8
 
+import pandas as pd
+
 if __name__ == '__main__':
     print('new reop!')
+
+    df = pd.DataFrame.from_records(
+        [{'concept': 'name', 
+        'name': 'Name', 
+        'concept_type': 'string'}])
+    df.to_csv('../../ddf--concepts.csv', index=False)

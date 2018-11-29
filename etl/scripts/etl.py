b3e283c2ad4e55843fb3f7d84edc382f0747cb30
semio <prairy.long@gmail.com>
2018-11-29 18:16:23 +0800

    use etl script..

--- etl/scripts/etl.py
+++ etl/scripts/etl.py
@@ -5,8 +5,28 @@ import pandas as pd
 if __name__ == '__main__':
     print('new reop!')
 
-    df = pd.DataFrame.from_records(
-        [{'concept': 'name', 
-        'name': 'Name', 
-        'concept_type': 'string'}])
-    df.to_csv('../../ddf--concepts.csv', index=False)
+    concepts = pd.DataFrame.from_records(
+        [{'concept': 'name',
+          'name': 'Name',
+          'concept_type': 'string'},
+         {'concept': 'dps',
+          'name': 'datapoints',
+          'concept_type': 'measure'},
+         {'concept': 'time',
+          'name': 'time',
+          'concept_type': 'time'},
+         {'concept': 'ent',
+          'name': 'entity',
+          'concept_type': 'entity_domain'}])
+    concepts.to_csv('../../ddf--concepts.csv', index=False)
+
+    entities = pd.DataFrame.from_records(
+        [{'ent': 'a', 'name': 'a'}]
+    )
+    entities.to_csv('../../ddf--entities--ent.csv', index=False)
+
+    datapoints = pd.DataFrame.from_records(
+        [{'ent': 'a', 'time': 1990, 'dps': 1}]
+    )
+
+    datapoints.to_csv('../../ddf--datapoints--dps--by--ent--time.csv', index=False)
